05554726f59e5e628586c288e0a84368bc900e42
lukachi <lukachisama@gmail.com>
2023-11-06 16:39:58 +0200

    add i18n, based on tailwind ui library and global config

---
+++ src/middleware.ts
@@ -0,0 +1,45 @@
+import { NextRequest } from 'next/server'
+import { createI18nMiddleware } from 'next-international/middleware'
+
+const DEFAULT_LOCALE = 'en'
+const ACCEPTED_LOCALES = [DEFAULT_LOCALE]
+
+type SupportedLocale = typeof ACCEPTED_LOCALES[number] | null
+
+const I18nMiddleware = createI18nMiddleware<typeof ACCEPTED_LOCALES>({
+  locales: [DEFAULT_LOCALE],
+  defaultLocale: DEFAULT_LOCALE,
+
+  urlMappingStrategy: 'rewrite',
+  resolveLocaleFromRequest: request => {
+    const locale = localeFromRequest(request) ?? DEFAULT_LOCALE
+
+    if (ACCEPTED_LOCALES.includes(locale)) return locale
+
+    return DEFAULT_LOCALE
+  },
+})
+
+export function middleware(request: NextRequest) {
+  return I18nMiddleware(request)
+}
+
+export const config = {
+  matcher: ['/((?!_next|.*\\..*).*)'],
+}
+
+function localeFromRequest(request: NextRequest) {
+  let locale = request.cookies.get('Next-Locale')?.value ?? null
+
+  if (!locale) {
+    locale = negotiateAcceptLanguage(request)
+  }
+
+  return locale
+}
+
+function negotiateAcceptLanguage(request: NextRequest) {
+  const header = request.headers.get('Accept-Language')
+  const locale = header?.split(',')?.[0]?.split('-')?.[0]
+  return locale ?? null
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -14,7 +16,69 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    // colors: {
+    //   backgroundPrimaryLight: '',
+    //   backgroundPrimaryMain: '',
+    //   backgroundPrimaryDark: '',
+    //
+    //   backgroundSecondaryLight: '',
+    //   backgroundSecondaryMain: '',
+    //   backgroundSecondaryDark: '',
+    //
+    //   backgroundTertiaryLight: '',
+    //   backgroundTertiaryMain: '',
+    //   backgroundTertiaryDark: '',
+    //
+    //   successLight: '',
+    //   successMain: '',
+    //   successDark: '',
+    //
+    //   errorLight: '',
+    //   errorMain: '',
+    //   errorDark: '',
+    //
+    //   warningLight: '',
+    //   warningMain: '',
+    //   warningDark: '',
+    //
+    //   infoLight: '',
+    //   infoMain: '',
+    //   infoDark: '',
+    //
+    //   primaryLight: '',
+    //   primaryMain: '',
+    //   primaryDark: '',
+    //
+    //   secondaryLight: '',
+    //   secondaryMain: '',
+    //   secondaryDark: '',
+    //
+    //   textPrimaryLight: '',
+    //   textPrimaryMain: '',
+    //   textPrimaryDark: '',
+    //
+    //   textPrimaryInvertLight: '',
+    //   textPrimaryInvertMain: '',
+    //   textPrimaryInvertDark: '',
+    //
+    //   textSecondaryLight: '',
+    //   textSecondaryMain: '',
+    //   textSecondaryDark: '',
+    //
+    //   textTertiaryLight: '',
+    //   textTertiaryMain: '',
+    //   textTertiaryDark: '',
+    //
+    //   borderPrimaryLight: '',
+    //   borderPrimaryMain: '',
+    //   borderPrimaryDark: '',
+    //
+    //   borderSecondaryLight: '',
+    //   borderSecondaryMain: '',
+    //   borderSecondaryDark: '',
+    // }
   },
-  plugins: [],
+  // darkMode: "class",
+  plugins: [nextui()],
 }
 export default config
