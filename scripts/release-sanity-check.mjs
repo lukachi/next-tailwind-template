988e3c1996eed035e36a7e526b77d0693c0624ae
lukachi <lukachisama@gmail.com>
2023-11-07 15:02:15 +0200

    add changelog, update rsc and add docker file

--- scripts/release-sanity-check.mjs
+++ scripts/release-sanity-check.mjs
@@ -64,7 +64,7 @@ function validateChangelogHasVersionOnTop() {
 }
 
 function validateChangelogAnchorsLegend() {
-  const baseRepoUrl = 'https://gitlab.com/rarimo/scan'
+  const baseRepoUrl = PACKAGE_JSON.repository.url.replace('.git', '')
   const anyReleaseTagRe = /## \[\d+\.\d+\.\d+((-rc|-x)\.\d+)?\] - \d{4}-\d{2}-\d{2}/gi
 
   const expectedAnchorsLegend =
